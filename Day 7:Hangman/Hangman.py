4abcade4e1aea3c00be175228757df24a8d0bae2
OrinaOisera <orinamark22@gmail.com>
2022-03-27 12:53:21 +0300

    Adss correct ascci art

--- Day 7:Hangman/Hangman.py
+++ Day 7:Hangman/Hangman.py
@@ -1,63 +1,64 @@
-#step 1
-HANGMANPICS = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
+# step 1
+stages = [  '''
   +---+
   |   |
   O   |
  /|\  |
  / \  |
       |
+=========''',
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''',
+ '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''',
+          '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''',
+          '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''',
+           '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''',
+            '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
 =========''']
-
-
-
-
-
 
 word_list = ["ardvark", "babbon", "camel"]
 
-#Randomy choose a word from the list and assing it to the variable chosen_word
+# Randomy choose a word from the list and assigning it to the variable chosen_word
 import random
 
 chosen_word = random.choice(word_list)
@@ -70,15 +71,22 @@ for i in range(word_length):
 print(display)
 
 end_of_game = False
-
+lives = 6
 while not end_of_game:
     guess = input("Enter your 1st guess:").lower()
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+    print(f"{''.join(display)}")
 
-    print(display)
     if "_" not in display:
         end_of_game = True
         print("You win")
+    print(lives)
+    print(stages[lives])
