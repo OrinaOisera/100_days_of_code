41f80407a4d30f27e1d7b7ac9af9e1b0cac1d397
OrinaOisera <orinamark22@gmail.com>
2022-04-24 12:07:28 +0300

    Adds a while loop to keep the pogramme running until the user terminates the code by typing no

--- Day8:Functions_with_inputs/Caeser-cipher2.py
+++ Day8:Functions_with_inputs/Caeser-cipher2.py
@@ -1,6 +1,5 @@
 from logo import logo
 
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u',
@@ -11,27 +10,30 @@ def caesar(start_text, shift_amount, cypher_direction):
     end_text = ""
     if cypher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
     print(f'The {cypher_direction}d text is {end_text}')
 
 
 print(logo)
-direction = input("Type  'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shift = shift % 26
-#Solves fro when a  user enters a shift that is greater than the number of letter in the alphabet
+should_continue = True
+
+while should_continue:
+    direction = input("Type  'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+# Solves fro when a  user enters a shift that is greater than the number of letter in the alphabet
 
 
-
-
-caesar(start_text=text, shift_amount=shift, cypher_direction=direction)
-
-
-
-
-
-
+    caesar(start_text=text, shift_amount=shift, cypher_direction=direction)
+    result = input("Type 'yes'  if you want to go again.Otherwise type 'no'. \n ")
+    if result == "no":
+        should_continue = False
+        print("Good bye")
