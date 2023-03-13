24b84685925d8a9d76dac3d044f2c29dfff31bb9
Oisera <orinamark22@gmail.com>
2023-03-13 23:46:25 +0300

    Redid the day in month function

--- Day10:Functions_with_outputs/Days_in_Month.py
+++ Day10:Functions_with_outputs/Days_in_Month.py
@@ -1,24 +1,24 @@
 
-#  2000 /400 = 5  (leap)
 def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                return True
+               return True
             else:
-              return False
+             return  False
         else:
-            return False
+          return True
     else:
-       return  False
+           return  False
 
-def days_in_month(year, month ):
-   month_days = [31, 28 , 31, 30 , 31, 30,31, 31, 30 ,31,30,31]
-   if is_leap(year) and month ==2:
-       return  29
-   return month_days[month- 1]
-
-
+def days_in_month(year,month):
+        if month >  12 or month < 1 :
+             return  "Invalid month"
+        month_days = [31, 28 , 31, 30 , 31, 30,31, 31, 30 ,31,30,31]
+        if is_leap(year) and month == 2:
+            return 29
+        else:
+             return  month_days[month - 1]
 
 year = int(input("Enter a year:"))
 month = int(input("Enter a month:"))
